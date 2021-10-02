5e23414586b4c06247c83e03683d020697b2a518
Bhargav <bhargavkadali39@gmail.com>
2021-10-02 19:54:28 +0530

    Update WhatsApp_automation2.py

--- WhatsApp_automation2.py
+++ WhatsApp_automation2.py
@@ -1,6 +1,11 @@
 import pywhatkit as pk
+from time import sleep
 
 pk.sendwhats_image(phone_no="+91xxxxxxxxxx",img_path='D:/your_path' , caption="yo sup",print_wait_time=True,tab_close=True)
+
+while x in range(7):
+  sleep(1)
+  print((x-7)+'Time remaining')
 
 search_key = input('Enter search keyword for youtube video ')
 pk.playonyt(topic)
