78fc02a3623c9fd7509e723f66417b7eb253dd8b
Bhargav <bhargavkadali39@gmail.com>
2021-10-02 20:23:36 +0530

    Update WhatsApp_automation2.py

--- WhatsApp_automation2.py
+++ WhatsApp_automation2.py
@@ -6,10 +6,23 @@ def drinks_break():
       sleep(1)
       print(7-x,end=' sec remaining\n')
 
-
 pk.sendwhats_image(phone_no="+91xxxxxxxxxx",img_path='D:/your_path' , caption="yo sup",print_wait_time=True,tab_close=True)
 
 drinks_break()
 
-search_key = str(input('Enter search keyword for youtube video '))
-pk.playonyt(search_key)
+yt_search_key = str(input('Enter search keyword for youtube video '))
+pk.playonyt(yt_search_key)
+
+drinks_break()
+
+google_search_key = str(input('Enter search keyword for google '))
+
+pk.search(google_search_key)
+
+drinks_break()
+
+info_search_key = str(input('Enter search keyword for relevant information '))
+
+pk.info(info_search_key)
+
+drinks_break()
