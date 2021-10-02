eb1411fa12da90fae81583a5db72b0542a747619
Bhargav <bhargavkadali39@gmail.com>
2021-10-02 20:04:55 +0530

    Update WhatsApp_automation2.py

--- WhatsApp_automation2.py
+++ WhatsApp_automation2.py
@@ -1,11 +1,15 @@
 import pywhatkit as pk
 from time import sleep
 
+def drinks_break():
+  for x in range(7):
+      sleep(1)
+      print(7-x,end=' sec remaining\n')
+
+
 pk.sendwhats_image(phone_no="+91xxxxxxxxxx",img_path='D:/your_path' , caption="yo sup",print_wait_time=True,tab_close=True)
 
-for x in range(7):
-  sleep(1)
-  print((x-7)+'Time remaining')
+drinks_break()
 
-search_key = input('Enter search keyword for youtube video ')
-pk.playonyt(topic)
+search_key = str(input('Enter search keyword for youtube video '))
+pk.playonyt(search_key)
