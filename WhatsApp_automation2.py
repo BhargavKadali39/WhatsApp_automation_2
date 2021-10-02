2cd54ea9741188792bab850761df077b025d4826
Bhargav <bhargavkadali39@gmail.com>
2021-10-02 19:48:29 +0530

    Update WhatsApp_automation2.py

--- WhatsApp_automation2.py
+++ WhatsApp_automation2.py
@@ -1,3 +1,3 @@
 import pywhatkit as pk
 
-pk.sendwhats_image(phone_no="+91xxxxxxxxxx",img_path='D:/your_path' , caption="yo sup",print_wait_time=True)
+pk.sendwhats_image(phone_no="+91xxxxxxxxxx",img_path='D:/your_path' , caption="yo sup",print_wait_time=True,tab_close=True)
