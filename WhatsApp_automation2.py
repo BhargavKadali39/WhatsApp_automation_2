90688c1b1a2871f3f5a1d0d0452a5f8428d12fff
Bhargav <bhargavkadali39@gmail.com>
2021-10-02 19:50:05 +0530

    Update WhatsApp_automation2.py

--- WhatsApp_automation2.py
+++ WhatsApp_automation2.py
@@ -1,3 +1,6 @@
 import pywhatkit as pk
 
 pk.sendwhats_image(phone_no="+91xxxxxxxxxx",img_path='D:/your_path' , caption="yo sup",print_wait_time=True,tab_close=True)
+
+search_key = input('Enter search keyword for youtube video ')
+pk.playonyt(topic)
