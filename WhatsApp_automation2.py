c1dfab7ec4d63eb9c08e2acc93d2f8c42e952a07
Bhargav <bhargavkadali39@gmail.com>
2021-10-02 20:00:04 +0530

    Update WhatsApp_automation2.py

--- WhatsApp_automation2.py
+++ WhatsApp_automation2.py
@@ -3,7 +3,7 @@ from time import sleep
 
 pk.sendwhats_image(phone_no="+91xxxxxxxxxx",img_path='D:/your_path' , caption="yo sup",print_wait_time=True,tab_close=True)
 
-while x in range(7):
+for x in range(7):
   sleep(1)
   print((x-7)+'Time remaining')
 
